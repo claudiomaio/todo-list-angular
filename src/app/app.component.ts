0562cbc834127d03388c46c9b7ba3a8f94fc7a5d
claudiomaio <claudiomaio14@gmail.com>
2021-11-29 10:50:43 +0000

    add-remove-restore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list-app';
+  public title  = 'My todo list';
+  public todoItems: string[] = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+  public newItemToAdd: string = '';
+  public removeItem(i:number) {
+    this.doneItems.push(this.todoItems[i]);
+    this.todoItems.splice(i, 1);
+  }
+  public addItem() {
+    this.todoItems.push(this.newItemToAdd);
+    this.newItemToAdd = '';
+  }
+  
+  public title2 = 'Already Done';
+  public doneItems: string[] = [];
+  public restoreItem(i:number) {
+    this.todoItems.push(this.doneItems[i]);
+    this.doneItems.splice(i, 1);
+  }
+  public deleteItem(i:number) {
+    this.doneItems.splice(i, 1);
+  }
 }
